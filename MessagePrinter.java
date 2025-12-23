51a8defd02c4c577864dbc35ae1be17ffc6c4a8d
kostakurgun-tech <kostakurgun@gmail.com>
2025-12-24 00:33:41 +0200

    Add goodbye message functionality

--- MessagePrinter.java
+++ MessagePrinter.java
@@ -8,9 +8,14 @@ public class MessagePrinter {
         return "Today is: " + java.time.LocalDate.now();
     }
 
+    public static String getGoodbyeMessage() {
+    	  return "Thank you for using MessagePrinter!";
+    }
+
     public static void main(String[] args) {
-        System.out.println(getWelcomeMessage());
-        System.out.println(getDateMessage());
-        System.out.println("Learning remote repositories!");
+    System.out.println(getWelcomeMessage());
+    System.out.println(getDateMessage());
+    System.out.println("Learning remote repositories!");
+    System.out.println(getGoodbyeMessage());
     }
 }
