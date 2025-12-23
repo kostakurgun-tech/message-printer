1892184e11eb9c2155d1aefc37c0952c7ec392cd
kostakurgun-tech <kostakurgun@gmail.com>
2025-12-24 01:18:17 +0200

    Add time message functionality

--- MessagePrinter.java
+++ MessagePrinter.java
@@ -13,9 +13,14 @@ public class MessagePrinter {
     	  return "Thank you for using MessagePrinter!";
     }
 
+    public static String getTimeMessage() {
+          return "Current time: " + java.time.LocalTime.now();
+    }
+
     public static void main(String[] args) {
     System.out.println(getWelcomeMessage());
     System.out.println(getDateMessage());
+    System.out.println(getTimeMessage());
     System.out.println("Learning remote repositories!");
     System.out.println(getGoodbyeMessage());
     }
